826d3bf2753d4ecdc533df6953e895e3e8aa4efd
jusski <jusski@sdf-eu.org>
2022-01-18 08:00:06 +0200

    Added some comments.

--- src/main/java/com/epam/training/student_justinas_skierus/java_io/optional_tasks/ChangeLowerCaseToUpperCase.java
+++ src/main/java/com/epam/training/student_justinas_skierus/java_io/optional_tasks/ChangeLowerCaseToUpperCase.java
@@ -13,14 +13,17 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 public class ChangeLowerCaseToUpperCase
 {
-
+	// NOTE(jusski) Java code can have \u1233 style symbols which are converted to
+	// symbols before parsing. What we must do in this program about them? After
+	// conversion it will not compile anyway so we could just pretend its not 1
+	// symbol but 5?
 	public static void main(String[] args) throws IOException
 	{
 		String path = args.length >= 1 ? args[0]
 				: "src/main/java/com/epam/training/student_justinas_skierus/java_io/optional_tasks/ChangeLowerCaseToUpperCase.java";
 		
+		// We are using codepoints, and not char array as to not check for high/low surrogate pairs. 
 		int[] codePoints = Files.readString(Paths.get(path)).codePoints().toArray();
-		
 		
 		for(int i = 0; i < codePoints.length - 2; ++i)
 		{
